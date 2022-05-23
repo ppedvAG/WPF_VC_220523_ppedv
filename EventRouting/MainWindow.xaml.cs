bdf44d0358dfb1c88a1df1bb9bc6f25d883cb44f
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-05-23 16:40:48 +0200

    PropertyBinding

---
+++ EventRouting/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EventRouting
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_KlickMich_Click(object sender, RoutedEventArgs e)
+        {
+            Tbl_Output.Text += $"{(sender as FrameworkElement)?.Name}: Click\n";
+        }
+
+        private void Wnd_Main_Click(object sender, RoutedEventArgs e)
+        {
+            Tbl_Output.Text += $"{(sender as FrameworkElement)?.Name}: ButtonBase-Click\n";
+        }
+
+        private void Spl_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Tbl_Output.Text += $"{(sender as FrameworkElement)?.Name}: Preview/Tunnel\n";
+        }
+
+        private void Spl_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Tbl_Output.Text += $"{(sender as FrameworkElement)?.Name}: Bubble\n";
+        }
+    }
+}
