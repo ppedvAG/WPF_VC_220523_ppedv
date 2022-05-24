43326c95008e2d4fbd42ff136fe67df1d9378c3e
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-05-24 16:30:20 +0200

    Validation

---
+++ Validation/IPv4Validation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Validation
+{
+    internal class IPv4Validation : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (Regex.IsMatch(value.ToString(), @"^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"))
+                return ValidationResult.ValidResult;
+            else
+                return new ValidationResult(false, "Bitte gib eine richtige IPv4-Adresse ein");
+        }
+    }
+}
---
+++ Validation/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Validation
+{
+    internal class Person : IDataErrorInfo
+    {
+        
+        public int Alter { get; set; }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case nameof(Alter):
+                        if (Alter < 0) return "Du kannst nicht jünger sein als 0 Jahre";
+                        if (Alter > 150) return "Du sollst nicht lügen";
+                        break;
+                    default:
+                        break;
+                }
+
+                return String.Empty;
+            }
+        }
+        public string Error => null;
+    }
+}
