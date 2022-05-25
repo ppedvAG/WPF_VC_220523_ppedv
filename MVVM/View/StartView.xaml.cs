7476731a2225806a8815398c67c24ad8e00e55fd
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2022-05-25 12:29:02 +0200

    MVVM

--- MVVM/View/StartView.xaml.cs
+++ MVVM/View/StartView.xaml.cs
@@ -10,17 +10,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MVVM
+namespace MVVM.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for StartView.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class StartView : Window
     {
-        public MainWindow()
+        public StartView()
         {
             InitializeComponent();
         }
